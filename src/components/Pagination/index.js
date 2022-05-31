07d66d4d6d5a57e25b1e98d566c1f1bea060f525
Qudret-Rustamli <qudret.rustem@gmail.com>
2022-05-31 19:00:22 +0400

    pagination finish

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,7 +1,28 @@
-import PageControl from "./PageControl";
-import usePagination from "./Pagination";
-import { useState } from "react";
-import { Pagination } from "@material-ui/lab";
+import PageControl from './PageControl';
+import usePagination from './Pagination';
+import { useState } from 'react';
+import { Pagination } from '@material-ui/lab';
+import styled from 'styled-components';
+
+const PaginationContainer = styled(Pagination)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 4.5rem;
+  & button {
+    border-radius: 50%;
+    width: 3.5rem !important;
+    height: 3.5rem !important;
+    background-color: transparent !important;
+    border: 1px solid ${({ theme }) => theme.bgPaginationBorder};
+    color: ${({ theme }) => theme.text};
+    font-weight: bold;
+  }
+  & .Mui-selected{
+    background-color: ${({ theme }) => theme.bgPaginationBorder }!important;
+  }
+
+`;
 
 export default function Pagi({ data, comp }) {
   let [page, setPage] = useState(1);
@@ -18,7 +39,7 @@ export default function Pagi({ data, comp }) {
   return (
     <>
       <PageControl comp={comp} datas={datas} />
-      <Pagination
+      <PaginationContainer
         count={count}
         size="large"
         page={page}
